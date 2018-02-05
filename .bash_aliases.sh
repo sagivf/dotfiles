5884e53d49e70c73993f288612a9cb41efad2b28
Sagiv Frankel <sagivf@gmail.com>
2018-02-05 23:36:25 +0200

    wip

--- .bash_aliases.sh
+++ .bash_aliases.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 alias n=nanobox
+alias ll='ls -l'
+alias lla='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
 
 ## Node
 export NVM_DIR="$HOME/.nvm"
@@ -8,7 +12,12 @@ export NVM_DIR="$HOME/.nvm"
 export NODE_ENV="development"
 
 
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
 ## Android
 export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+export RETHINK_NAME=spikli
